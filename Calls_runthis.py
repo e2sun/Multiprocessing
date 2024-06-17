4fb392ba33e7a13de21d7d74e78b409cdd0ca29b
Sun <esun@WNE-170443.bnl.gov>
2024-06-17 16:38:45 -0400

    Compare serial and multiprocessing using time

--- Calls_runthis.py
+++ Calls_runthis.py
@@ -1,6 +1,7 @@
 #bash stands for "born again shell"
 from multiprocessing import Pool
 import subprocess
+import time
 
 
 
@@ -11,18 +12,39 @@ def run_bash(filenumber):
     subprocess.call(to_bash)
 
 def do_mp(filenumbers):
+    
+
     p=Pool()
-    result=p.map(run_bash, filenumbers)
-
-
+    p.map(run_bash, filenumbers)
+    
     p.close()
     p.join()
 
+    
+
+    
+
+
+
+def no_mp(filenumbers):
+    for filenumber in filenumbers:
+        run_bash(filenumber)
+    
 
 
 
 
 if __name__=='__main__':
-    filenumbers=list(map(str,range(5)))
+    filenumbers=list(map(str,range(10)))
 
+    start_time1=time.time()
     do_mp(filenumbers)
+    end_time1 = time.time()-start_time1
+   
+
+    start_time2=time.time()
+    no_mp(filenumbers)
+    end_time2 = time.time()-start_time2
+
+    print("Processing " + str(len(filenumbers)) + " files took " + str(end_time1) + " time using multiprocessing.")
+    print("Processing " + str(len(filenumbers)) + " files took " + str(end_time2) + " time using serial processing.")
